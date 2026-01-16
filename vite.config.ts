7b81fc86445b883fbc40aef1bfc5a05d5c668f14
felipemacchiz <felipe.macchiz@gmail.com>
2026-01-16 15:07:00 -0300

    pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,16 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pokédex',
+        short_name: 'Pokedex',
+        description: 'Sua pokédex!',
+        theme_color: '#173EA5',
+        background_color: '#ffffff',
+        display: 'standalone',
+      }
+    }),
   ],
 })
